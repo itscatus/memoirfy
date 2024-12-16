bec393249b85c69fd993ad42092cae5fb3878a7a
fel <100121123+itscatus@users.noreply.github.com>
2024-12-16 11:01:35 +0700

    Update DiaryList.vue

--- memoirfy/src/views/DiaryList.vue
+++ memoirfy/src/views/DiaryList.vue
@@ -25,7 +25,7 @@
   import { getAllDiaries, deleteDiary, updateDiary } from "../services/diaryService";
   import PocketBase from 'pocketbase';
 
-const pb = new PocketBase('http://127.0.0.1:8090');
+const pb = new PocketBase('https://memoirfy.pockethost.io/');
 
  
   export default {
