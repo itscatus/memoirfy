6bb2f16633b72a0dfda997d101c08662fa274ee2
fel <100121123+itscatus@users.noreply.github.com>
2024-12-16 11:00:56 +0700

    Update Register.vue

--- memoirfy/src/views/Register.vue
+++ memoirfy/src/views/Register.vue
@@ -25,7 +25,7 @@
   
   <script>
   import PocketBase from "pocketbase";
-  const pb = new PocketBase("http://127.0.0.1:8090");
+  const pb = new PocketBase("https://memoirfy.pockethost.io/);
   
   export default {
     data() {
